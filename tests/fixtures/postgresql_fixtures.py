406f4a953d98caf2c901e477c0d533cd9c43b002
Simone Porreca <porrecasimone@gmail.com>
2025-10-10 20:25:28 +0200

    Create postgresql_fixtures.py

---
+++ tests/fixtures/postgresql_fixtures.py
@@ -0,0 +1,57 @@
+"""
+The module includes Fixtures related to the module "postgresql_connector".
+"""
+
+# Import Standard Libraries
+import os
+import pathlib
+from dynaconf import Dynaconf
+import pytest
+
+# Import Package Modules
+from data_grimorium.postgresql_connector.postgresql_types import PostgreSQLClientConfig
+from data_grimorium.postgresql_connector.postgresql_connector import PostgreSQLConnector
+
+
+# Retrieve the root path
+root_path = os.getenv("DRUIDIC_GROVE_AI_ROOT_PATH")
+
+# Read the configuration file
+config = Dynaconf(
+    settings_files=[pathlib.Path(root_path) / "configuration" / "stackoverflow_settings.toml"],
+    environments=True,
+    env="pytest",
+)
+
+
+@pytest.fixture
+def fixture_postgresql_client_config(
+    client_config: dict = config["postgresql"]["client"],
+) -> PostgreSQLClientConfig:
+    """
+    Fixture for a PostgreSQLClientConfig object
+    from src/postgresql_connector/postgresql_types.py.
+
+    Args:
+        client_config (Dictionary): Configurations for a PostgreSQLClientConfig object
+
+    Returns:
+        (PostgreSQLClientConfig): Object of PostgreSQL client configurations
+    """
+    return PostgreSQLClientConfig(**client_config)
+
+
+@pytest.fixture
+def fixture_postgresql_connector(
+    fixture_postgresql_client_config: PostgreSQLClientConfig,
+) -> PostgreSQLConnector:
+    """
+    Fixture for a PostgreSQLConnector object in order to connect to a PostgreSQL Database.
+
+    Args:
+        fixture_postgresql_client_config (PostgreSQLClientConfig): Client configurations
+
+    Returns:
+        (PostgreSQLConnector): Object of PostgreSQL Connector
+    """
+    return PostgreSQLConnector(client_config=fixture_postgresql_client_config)
